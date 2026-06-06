635a01386652bf615253a81cdd712faf42d09071
Priyanshu Ojha <78288166+Priyannshu@users.noreply.github.com>
2026-06-07 02:23:37 +0530

    Truncate apikey and authorization from comments

--- test.py
+++ test.py
@@ -3,8 +3,8 @@
 # # Test the API directly
 # url = "https://kwztlhywsczfajjtpjmo.supabase.co"
 # headers = {
-#     "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3enRsaHl3c2N6ZmFqanRwam1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxNzk5NjMsImV4cCI6MjA3Mjc1NTk2M30.vjr315rV_HkZkVVpPmhqc4eNgWKcRLowOf_8-osH1KU",
-#     "Authorization": f"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3enRsaHl3c2N6ZmFqanRwam1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxNzk5NjMsImV4cCI6MjA3Mjc1NTk2M30.vjr315rV_HkZkVVpPmhqc4eNgWKcRLowOf_8-osH1KU"
+#     "apikey": "eyJhbGciOiJIUzI1NiIsjA3Mjc1NTk2M30.vjr315rV_HkZkVVpPmhqc4eNgWKcRLowOf_8-osH1KU",
+#     "Authorization": f"eyJ9sZSI6ImFub24iLCJpYXQiOjE3NTcxNzk5NjMsImV4cCI6MjA3Mjc1NTk2M30.vjr315rV_HkZkVVpPmhqc4eNgWKcRLowOf_8-osH1KU"
 # }
 #
 # response = requests.get(url, headers=headers)
